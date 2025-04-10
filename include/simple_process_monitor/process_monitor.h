0d68d3493295ae30331abbcf8a242107bc2b4aa2
Mike Jiang <jyt19960208@gmail.com>
2025-04-11 02:06:18 +0800

    Add logger options

--- include/simple_process_monitor/process_monitor.h
+++ include/simple_process_monitor/process_monitor.h
@@ -5,9 +5,12 @@
 #include <unistd.h>
 
 #include <chrono>
+#include <cstdio>
+#include <thread>
 #include <vector>
 
-#include "process_tree_wrapper.h"
+#include "simple_process_monitor/process_tree_wrapper.h"
+#include "simple_process_monitor/system_info.h"
 
 namespace simple_process_monitor {
 
@@ -23,17 +26,114 @@ public:
     ProcessMonitor(const ProcessMonitor &other) = delete;
     ProcessMonitor &operator=(const ProcessMonitor &other) = delete;
 
-    void logTopCpu() const;
+    void logTopCpu() const {
+        logTopCpu(printf);
+    }
 
-    void logTopRam() const;
+    template <typename LOGGER_TYPE>
+    void logTopCpu(LOGGER_TYPE &logger) const {
+        TopProcessInfos topProcessInfos = collectTopInfo(TopInfoType::CPU);
+        TopProcessThreadInfos topProcessThreadInfos;
+
+        if (pid_ == ALL_PROCESSES) {
+            topProcessThreadInfos.resize(topProcessInfos.size());
+
+            std::vector<std::thread> threads;
+
+            for (unsigned long i = 0; i < topProcessInfos.size(); i++) {
+                auto &process = topProcessInfos[i];
+
+                threads.emplace_back([pid = process.pid,
+                                      monitorInterval = monitorInterval_,
+                                      logCount = logCount_,
+                                      &topProcessThreadInfos,
+                                      i]() {
+                    ProcessMonitor pm{pid, monitorInterval, logCount};
+                    topProcessThreadInfos[i] = pm.collectTopInfo(TopInfoType::CPU);
+                });
+            }
+
+            for (auto &t : threads) {
+                t.join();
+            }
+
+            logger("Top %lu of system processes' CPU usages\n", topProcessInfos.size());
+        } else {
+            logger("Top %lu of process pid %d's threads' CPU usages\n", topProcessInfos.size(), pid_);
+        }
+
+        logger("------------------------------------------------------------\n");
+
+        for (unsigned long i = 0; i < topProcessInfos.size(); i++) {
+            logger("%d  %.1f %%  %s\n",
+                   topProcessInfos[i].pid,
+                   topProcessInfos[i].cpuUsage,
+                   topProcessInfos[i].cmdline.c_str());
+
+            logger("------------------------------------------------------------\n");
+
+            if (pid_ == ALL_PROCESSES) {
+                for (auto &thread : topProcessThreadInfos[i]) {
+                    logger("%d  %.1f %%  %s\n", thread.pid, thread.cpuUsage, thread.cmdline.c_str());
+                }
+
+                logger("------------------------------------------------------------\n");
+            }
+        }
+
+        logger("\n");
+    }
+
+    void logTopRam() const {
+        logTopRam(printf);
+    }
+
+    template <typename LOGGER_TYPE>
+    void logTopRam(LOGGER_TYPE &logger) const {
+        TopProcessInfos topProcessInfos = collectTopInfo(TopInfoType::RAM);
+
+        if (pid_ == ALL_PROCESSES) {
+            logger("Top %lu processes' RAM usages (total %.1f MiB)\n",
+                   topProcessInfos.size(),
+                   static_cast<double>(g_fixed_system_info.memory_size) / (1024 * 1024));
+        } else {
+            logger("Process pid %d's RAM usage (total %.1f MiB)\n",
+                   pid_,
+                   static_cast<double>(g_fixed_system_info.memory_size) / (1024.0 * 1024.0));
+        }
+
+        logger("------------------------------------------------------------\n");
+
+        for (unsigned long i = 0; i < topProcessInfos.size(); i++) {
+            if (pid_ != ALL_PROCESSES && i == 1) {
+                break;
+            }
+
+            logger("%d  %.1f MiB  %s\n",
+                   topProcessInfos[i].pid,
+                   static_cast<double>(topProcessInfos[i].ramUsage) / (1024 * 1024),
+                   topProcessInfos[i].cmdline.c_str());
+
+            logger("------------------------------------------------------------\n");
+        }
+
+        logger("\n");
+    }
 
 private:
     using TopProcessInfos = std::vector<ProcessOrThreadInfo>;
-    using OneProcessThreadInfos = TopProcessInfos;
-    using TopProcessThreadInfos = std::vector<OneProcessThreadInfos>;
-    using LogInfo = std::pair<TopProcessInfos, TopProcessThreadInfos>;
+    using TopProcessThreadInfos = std::vector<TopProcessInfos>;
 
-    void collectTopInfo(TopInfoType type, TopProcessInfos &threadInfos) const;
+    TopProcessInfos collectTopInfo(TopInfoType type) const {
+        ProcessTreeWrapper processTreeWrapper{pid_};
+
+        if (type == TopInfoType::CPU) {
+            std::this_thread::sleep_for(monitorInterval_);
+            processTreeWrapper.update();
+        }
+
+        return processTreeWrapper.getTopProcessInfos(type, logCount_);
+    }
 
     const pid_t pid_;
     const std::chrono::seconds monitorInterval_;
--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,14 @@
 #include <cassert>
+#include <cstdarg>
 #include <cstdio>
 #include <thread>
 
 #include "simple_process_monitor/process_monitor.h"
-#include "simple_process_monitor/system_info.h"
 
 static void testSystemInfo() {
     using namespace std::chrono_literals;
 
-    printf("CPU count is %d, RAM is %.1f MiB, hz is %.1f, page_size is %ld B\n",
+    printf("System CPU count is %d, RAM is %.1f MiB, hz is %.1f, page_size is %ld B\n",
            g_fixed_system_info.cpu_count,
            static_cast<double>(g_fixed_system_info.memory_size) / (1024 * 1024),
            g_fixed_system_info.hz,
@@ -23,9 +23,23 @@ static void testSystemInfo() {
 
     assert(update_system_info(&systemInfo));
 
-    printf("CPU usage is %.1f %% / 100 %%\n", systemInfo.cpu.usage.user + systemInfo.cpu.usage.system);
+    printf("System CPU usage is %.1f %% / 100 %%\n", systemInfo.cpu.usage.user + systemInfo.cpu.usage.system);
     printf("\n");
 }
+
+struct TestLogger {
+    int operator()(const char *fmt, ...) {
+        int ret = printf("TestLogger output: ");
+
+        va_list ap;
+
+        va_start(ap, fmt);
+        ret += vprintf(fmt, ap);
+        va_end(ap);
+
+        return ret;
+    }
+};
 
 static void testProcessMonitor() {
     using namespace simple_process_monitor;
@@ -39,9 +53,10 @@ static void testProcessMonitor() {
 
     {
         ProcessMonitor pmOneProcess(1);
+        TestLogger logger;
 
-        pmOneProcess.logTopCpu();
-        pmOneProcess.logTopRam();
+        pmOneProcess.logTopCpu(logger);
+        pmOneProcess.logTopRam(logger);
     }
 
     {
